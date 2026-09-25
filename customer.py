3343316a12cf069832c1adfbbc298175aa4a159f
Sumit Yagik <151998385+Sumityagik@users.noreply.github.com>
2026-09-25 17:29:42 +0530

    Fix SQL insert statement formatting

--- customer.py
+++ customer.py
@@ -209,28 +209,26 @@ class cust_win:
         
             conn=mysql.connector.connect(host="localhost",username="root",password="vaibhav@2026",database="management")
             my_cursor=conn.cursor()
-            my_cursor.execute("insert into customer values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s"),(self.var_ref.get(),
-                                                                                               self.var_cust_name.get(),
-                                                                                               self.var_mother.get(),
-                                                                                               self.var_gender.get(),
-                                                                                               self.var_post.get(),
-                                                                                               self.var_mobile.get(),
-                                                                                               self.var_email.get(),
-                                                                                               self.var_nationality.get(),
-                                                                                               self.var_idproof.get(),
-                                                                                               self.var_idnumber.get(),
-                                                                                               self.var_address.get()
-                                                                                               )
+            my_cursor.execute("insert into customer values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s"),
+                (self.var_ref.get(),
+               self.var_cust_name.get(),
+               self.var_mother.get(),
+               self.var_gender.get(),
+               self.var_post.get(),
+               self.var_mobile.get(),
+               self.var_email.get(),
+               self.var_nationality.get(),
+               self.var_idproof.get(),
+               self.var_idnumber.get(),
+               self.var_address.get()
+               )
             
-      
-                                                                                              
-                                                                                        
             conn.commit()
             conn.close()
             messagebox.showinfo("Success","customer has been added",parent=self.root)
             
             except Exception as es:
-             messagebox.showwarning("warning",f"something went wrong:{str()}",parent=self.root)   
+                messagebox.showwarning("warning",f"something went wrong:{str()}",parent=self.root)   
 
 
 
